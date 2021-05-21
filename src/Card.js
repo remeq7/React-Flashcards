4e329d96614c25dd3eb5674e389106164bb49019
remeq7 <remremo20a@gmail.com>
2021-05-21 11:38:00 +0200

    first commit
    react flashcards

--- src/Card.js
+++ src/Card.js
@@ -1,20 +1,26 @@
-# Description
+import React, { useState } from "react";
+import questions from "./data";
+import {
+  FaArrowLeft,
+  FaArrowRight,
+  FaChevronCircleDown,
+  FaChevronCircleUp,
+} from "react-icons/fa";
 
-React learning cards.
+function Card() {
+  const [index, setIndex] = useState(0);
+  const [expand, setExpand] = useState(false);
 
-# Code
+  const checkNumber = (number) => {
+    if (number > questions.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return questions.length - 1;
+    }
+    return number;
+  };
 
-```javascript
-//example data
-const questions = [
-  {
-    id: 1,
-    question: "Sposoby definiowania komponentów w React",
-    text: "klasa ES6, komponent bez stanu (ang. state) za pomocą funkcji, składnia ES5 za pomocą pakietu npm create-react-class,  stan dzięki Hakom (ang. Hooks)",
-  },
-});
-
-//next question
   const nextQuestion = () => {
     setIndex((index) => {
       const newIndex = index + 1;
@@ -25,8 +31,17 @@ const questions = [
     });
   };
 
-  //JSX
-    return (
+  const prevQuestion = () => {
+    setIndex((index) => {
+      const newIndex = index - 1;
+      if (expand === true) {
+        setExpand(!expand);
+      }
+      return checkNumber(newIndex);
+    });
+  };
+
+  return (
     <div className="container">
       <article className="card">
         <h4>{questions[index].question}</h4>
@@ -49,9 +64,6 @@ const questions = [
       </article>
     </div>
   );
-```
+}
 
-## View
-
-![Alt text](screen1.png "Title")
-![Alt text](screen2.png "Title")
+export default Card;
---
+++ src/data.js
@@ -0,0 +1,24 @@
+const questions = [
+  {
+    id: 1,
+    question: "Sposoby definiowania komponentów w React",
+    text: "klasa ES6, komponent bez stanu (ang. state) za pomocą funkcji, składnia ES5 za pomocą pakietu npm create-react-class,  stan dzięki Hakom (ang. Hooks)",
+  },
+  {
+    id: 2,
+    question: "Co to są komponenty wyższego rzędu?",
+    text: "Komponenty wyższego rzędu (ang. Higher Order Component - HOC), zasada jest podobna do funkcji wyższego rzędu. Komponenty tego typu do funkcje, które przyjmują komponenty jako argument oraz zwracają nowy argument. Przykładem może być funkcja connect z biblioteki Redux.",
+  },
+  {
+    id: 3,
+    question: "Co to są render props?",
+    text: "Render props są to właściwości komponentu, które zawierają funkcje, która otrzymuje stan komponentu lub jakieś inne dane i zwraca kod jsx",
+  },
+  {
+    id: 4,
+    question: "Jak działa JSX?",
+    text: "Kompilator JSX, np. Babel parsuje kod JavaScript (JSX), znajduje wszystkie odwołania do tagów html i zastępuje je wywołaniem funkcji React.createElement lub w przypadku, gdy nazwa tagu jest z dużej litery, kompilator używa zmiennej komponentu.",
+  },
+];
+
+export default questions;
